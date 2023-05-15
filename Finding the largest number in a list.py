c581f59e22c571707541b558b3c58dcea1d92d5f
Deborah-offiong <130248510+Deborah-offiong@users.noreply.github.com>
2023-05-15 16:08:49 +0100

    Update Finding the largest number in a list.py
    A code to reverse a given number

--- Finding the largest number in a list.py
+++ Finding the largest number in a list.py
@@ -1,3 +1,4 @@
+#code to find the largest number
 largestSoFar = 2
 print("Smallest number is", largestSoFar)
 for the_num in [2,6,3,9,41,67,8] :
@@ -5,3 +6,19 @@ for the_num in [2,6,3,9,41,67,8] :
          largestSoFar=the_num
     print(largestSoFar, the_num)   
 print("Largest number is", largestSoFar)
+
+#A program to  reverse a given number
+number= int(input("enter a number to be reversed: "))
+reversed = 0
+while(number>0):
+    digit= number % 10
+    reverse = (reversed * 10) + digit
+    number = number // 10
+print("The reversed number is:", reverse)
+number= int(input("enter a number to be reversed: "))
+reversed = 0
+while(number>0):
+    digit= number % 10
+    reverse = (reversed * 10) + digit
+    number = number // 10
+print("The reversed number is:", reverse)
